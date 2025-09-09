b5409e7e52329f810dfccf4d445fa4f4aa17ee9d
Dylan Ophir <dmophir@gmail.com>
2025-09-09 01:57:36 -0400

    blind replace 00be with 00bc

--- libtudor/download_driver.sh
+++ libtudor/download_driver.sh
@@ -9,13 +9,15 @@ mkdir -p "$TMP_DIR"
 
 #Download the driver executable and check hash
 INSTALLER="$TMP_DIR/installer.exe"
-wget https://download.lenovo.com/pccbbs/mobiles/r19fp02w.exe -O "$INSTALLER"
+wget --user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36" \
+    https://dl.dell.com/FOLDER12308597M/1/Synaptics-Fingerprint-Sensor-Driver_3PFJG_WIN64_6.0.18.1103_A06.EXE \
+    -O "$INSTALLER"
 shasum "$INSTALLER" | cut -d" " -f1 | cmp - "$HASH_FILE"
 
 #Extract the driver
 WINDRV="$TMP_DIR/windrv"
 mkdir -p "$WINDRV"
-innoextract -d "$WINDRV" "$INSTALLER"
+unzip -d "$WINDRV" "$INSTALLER"
 
 #Copy outputs
 mkdir -p "$OUT_DIR"
